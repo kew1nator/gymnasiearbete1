d7ea9877d2f7e94dd9fc2e08e200bad42b7fb98a
Kevin Lindström <0007keli@res.ludvika.intra>
2019-01-29 15:31:33 +0100

    e28

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ const app = express();
 app.get('/', (request, response) =>{
 response.send('hello world');
 });
-
-app.listen(1111, () => console.log('application running on port 1111'));
+app.get("/sida2",(request, response) =>{
+    response.send('sida2')
+});
+app.listen(1111, () => console.log('application running on port 1111/sida2'));
